2af9f9c1052d9e1990c9aab3a9e9bceea18463a0
Gabriel Nunes <gabrielchagasn@gmail.com>
2025-12-11 23:04:54 -0300

    Teste

--- 0x04/entities_JPA+H2_database/src/main/java/com/example/jpa_h2_demo/model/Cliente.java
+++ 0x04/entities_JPA+H2_database/src/main/java/com/example/jpa_h2_demo/model/Cliente.java
@@ -13,20 +13,10 @@ public class Cliente {
     private String nome;
     private String email;
 
-    // Um cliente tem vários telefones
-    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "cliente")
     private List<Telefone> telefones;
 
-    // Um cliente tem vários endereços
-    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "cliente")
     private List<Endereco> enderecos;
 
-    public Cliente() {}
-
-    public Cliente(String nome, String email) {
-        this.nome = nome;
-        this.email = email;
-    }
-
-    // getters e setters omitidos para brevidade
 }
